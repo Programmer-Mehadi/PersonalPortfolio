0b86359436969cb47e95d189cfdb1180e7cad179
mirzaistiakhislam <mirza15-9603@diu.edu.bd>
2022-12-11 00:02:44 +0600

    error fixed.

--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -21,13 +21,13 @@ const Navbar = () => {
                     </div>
                     <div className='navbar-end flex-1 w-fit'>
                         <div className="dropdown ">
+
+                            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-lg font-semibold ml-[-200px]">
+                                {liList}
+                            </ul>
                             <label tabIndex={0} className="w-fit   btn btn-ghost lg:hidden">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                             </label>
-                            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-lg font-semibold ml-[-200px]">
-                                {liList}
-
-                            </ul>
                         </div>
                     </div>
                 </div>
--- src/pages/Home/HomeSection/HomeSection.js
+++ src/pages/Home/HomeSection/HomeSection.js
@@ -7,56 +7,41 @@ import twitter from '../../../assets/twitter.png';
 const HomeSection = () => {
     return (
         <div id='home' className='min-h-[600px] flex justify-center items-center py-14 lg:py-0' >
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-8  max-w-[1440px] w-[97%] mx-auto '>
-                <div>
-                    <h4 className='text-lg font-semibold'>Hi I'm Muhammad Mehadi</h4>
-                    <h1 className='text-3xl my-4 font-bold'>I'm <span className='text-[#3B3486]'>Web Developer</span></h1>
+            <div className='flex flex-col  md:flex-row  gap-8  max-w-[1440px] w-[97%] mx-auto  '>
+                <div className='order-2 '>
+                    <h4 className='text-center md:text-left text-lg font-semibold'>Hi I'm Muhammad Mehadi</h4>
+                    <h1 className='text-center md:text-left text-3xl my-4 font-bold'>I'm <span className='text-[#3B3486]'>Web Developer</span></h1>
                     <p className='text-lg'>As a passionate Web Developer and Javascript Expert. I have experience in HTML, CSS, and JavaScript,React,Expressjs and Mongodb. Combined with my recent projects in full stack web development, I am confident I have the skills to help you.</p>
                     <div className='my-8'>
-                        <div className='flex gap-4 flex-wrap'>
+                        <div className='flex justify-center md:justify-start  gap-4 flex-wrap'>
                             <a href='#projects' className="btn bg-[#3B3486] rounded-[3px] px-8">View Projects</a>
                             <a href="Md Zahirul Islam Mehadi's Resume.pdf" className="btn bg-[#CB1C8D]  text-white border-[#3B3486]  rounded-[3px] hover:text-white px-8">Download RESUME</a>
                         </div>
                     </div>
-                    <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
-                        <div className='grid grid-cols-1 gap-6'>
-                            <h2 className='font-bold'>Find With Me</h2>
-                            <div className='social-list flex gap-4'>
-                                <a href={`https://www.linkedin.com/in/muhammad-mehadi/`} className='bg-[#f0eff4] p-2 rounded-[3px]'>
-                                    <img  src={linkedin} alt="" />
-                                </a>
-                                <a href={`https://www.facebook.com/md.mehadi.talukder/`} className='bg-[#f0eff4] p-2 rounded-[3px]'>
-                                    <img src={facebook} alt="" />
-                                </a>
-                                <a href={`https://twitter.com/MuhammadMehadi`} className='bg-[#f0eff4] p-2 rounded-[3px]'>
-                                    <img src={twitter} alt="" />
-                                </a>
-                            </div>
+                    <div className='flex flex-col gap-6 items-center md:items-start'>
+                        <h2 className='font-bold'>Find With Me</h2>
+                        <div className='social-list flex gap-4'>
+                            <a href={`https://www.linkedin.com/in/muhammad-mehadi/`} className='bg-[#f0eff4] p-2 rounded-[3px]'>
+                                <img src={linkedin} alt="" />
+                            </a>
+                            <a href={`https://www.facebook.com/md.mehadi.talukder/`} className='bg-[#f0eff4] p-2 rounded-[3px]'>
+                                <img src={facebook} alt="" />
+                            </a>
+                            <a href={`https://twitter.com/MuhammadMehadi`} className='bg-[#f0eff4] p-2 rounded-[3px]'>
+                                <img src={twitter} alt="" />
+                            </a>
                         </div>
-                        {/* <div className='grid grid-cols-1 gap-6'>
-                            <h2 className='font-bold'>Best Skill On</h2>
-                            <div className='social-list flex gap-4'>
-                                <div className='bg-[#f0eff4] p-2 rounded-[3px]'>
-                                    <img src={linkedin} alt="" />
-                                </div>
-                                <div className='bg-[#f0eff4] p-2 rounded-[3px]'>
-                                    <img src={facebook} alt="" />
-                                </div>
-                                <div className='bg-[#f0eff4] p-2 rounded-[3px]'>
-                                    <img src={twitter} alt="" />
-                                </div>
-                            </div>
-                        </div> */}
+
                     </div>
                 </div>
-                <div className='flex justify-center lg:justify-end'>
+                <div className='sm:order-first md:order-last flex justify-center lg:justify-end'>
                     <div className=' rounded-[100%] h-[300px] w-[300px]  md:h-[400px] md:w-[400px] border-4 border-[#3B3486]'>
                         <img className=' rounded-[100%] h-[300px] w-[300px]  md:h-[400px] md:w-[400px] border-0 border-[#3B3486] mt-[10px]' src={mehadi} alt="" />
                     </div>
                 </div>
 
             </div>
-        </div>
+        </div >
 
     );
 };
